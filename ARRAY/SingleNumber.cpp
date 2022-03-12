c94be024e5e2a8e02883dfe35630f4c6fa328a66
kanhaiya <kumar.kanhaiya2206@gmail.com>
2022-03-12 23:50:56 +0530

    Single Number

---
+++ ARRAY/SingleNumber.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int singleNumber(vector<int> nums) {
+       unordered_map<int,int> m;//Time:O(n) Space:O(n)
+        for(auto i:nums)
+            m[i]++;
+        for(auto j:m)
+            if(j.second==1)
+                return j.first;
+            return -1;
+         
+    }
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int> nums(n);
+    for(int i=0;i<n;i++)
+    {
+      cin>>nums[i];
+    }
+    int ans=singleNumber(nums);
+    cout<<ans<<endl;
+    return 0;
+}    
