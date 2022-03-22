4fc400102d8e2a6377e8e9bccb79a8edec9d15a2
kanhaiya <kumar.kanhaiya2206@gmail.com>
2022-03-22 23:06:11 +0530

    Number of Good Pairs

--- ARRAY/No.ofGoodPairs.cpp
+++ ARRAY/No.ofGoodPairs.cpp
@@ -1,6 +1,7 @@
 //Given an array of integers nums, return the number of good pairs.
 //A pair (i, j) is called good if nums[i] == nums[j] and i < j.LeetCode:1512
 #include<bits/stdc++.h>
+
 using namespace std;
 
  int numIdenticalPairs(vector<int>& nums) {
