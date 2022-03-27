7df4baf6fb7ea54b642d16f9845c77daf87e0622
kanhaiya <kumar.kanhaiya2206@gmail.com>
2022-03-27 11:44:20 +0530

    K Weakest Rows in a Matrix

---
+++ ARRAY/WeakestRows.cpp
@@ -0,0 +1,24 @@
+//LeetCode:1337
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        int n=mat.size();
+        set<pair <int,int> > s;
+        for(int i=0;i<n;i++)
+        {
+            int cnt=count(mat[i].begin(),mat[i].end(),1);
+            s.insert({cnt,i});
+        }
+        vector<int> ans;
+        for(auto i:s)
+        {
+            if(k==0)
+                break;
+            ans.push_back(i.second);
+            --k;
+        }
+        return ans;
+    }
+};
