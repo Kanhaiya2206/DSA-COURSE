3a9e14691dbf2da84ea030ad143e05cf4f0a3b65
kanhaiya <kumar.kanhaiya2206@gmail.com>
2022-03-28 23:54:24 +0530

    Search In Rotated Sorted Array I

---
+++ BINARY SEARCH/RotatedSortedArrayI.cpp
@@ -0,0 +1,33 @@
+//LeetCode:33
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int low=0;
+        int high=n-1;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[low]<=nums[mid])
+            {
+                if(target<nums[mid] && target>=nums[low])
+                    high=mid-1;
+                else
+                    low=mid+1;
+            }
+            else
+            {
+                if(target>nums[mid] && target<=nums[high])
+                    low=mid+1;
+                else
+                    high=mid-1;
+            }
+            
+        }
+        return -1;
+        
+    }
+};
