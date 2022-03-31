da000e722194d99f448445d41cc26ddee0f5c831
kanhaiya <kumar.kanhaiya2206@gmail.com>
2022-03-31 22:25:45 +0530

    Reverse String

---
+++ STRING/ReverseString.cpp
@@ -0,0 +1,18 @@
+//LeetCode:344
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n= s.size();
+        int l=0;
+        int h=n-1;
+        while(l<h)
+        {
+            swap(s[l],s[h]);
+            l++;
+            h--;
+        }
+        
+    }
+};
