ee5d869ce4182b4707bac72ded0ab1a901210aa3
kanhaiya <kumar.kanhaiya2206@gmail.com>
2022-04-12 00:33:03 +0530

    Shift 2D Grid

--- ARRAY/Shift2DGrid.cpp
+++ ARRAY/Shift2DGrid.cpp
@@ -1,8 +1,23 @@
+//LeetCode:1260
 #include<bits/stdc++.h>
 using namespace std;
 class Solution {
 public:
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+         int m=grid.size();
+        int n=grid[0].size();
+        vector<vector<int>> ans(m,vector<int>(n,0));
         
+        for(int r=0;r<m;r++)
+        {
+            for(int c=0;c<n;c++)
+            {
+                int newindex=((r*n + c)+k) % (m*n);
+                int newr=newindex/n;
+                int newc=newindex%n;
+                ans[newr][newc]=grid[r][c];
+            }
+        }
+        return ans;
     }
 };
