2417fd1c500b7b3ca57f02f000760a9e46896eac
kanhaiya <kumar.kanhaiya2206@gmail.com>
2022-05-02 23:49:16 +0530

    Sort Array by Parity

---
+++ ARRAY/Sort Array by Parity.cpp
@@ -0,0 +1,19 @@
+//LeetCode:905
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int j=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]%2==0)
+            {
+            swap(nums[i],nums[j]);
+            j++;
+            }
+        }
+        return nums;
+        
+    }
+};
