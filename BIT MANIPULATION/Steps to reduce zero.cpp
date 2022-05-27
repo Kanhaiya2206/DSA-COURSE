5d26e6d783556dad7661e799554c5d7b5cb2004c
kanhaiya <kumar.kanhaiya2206@gmail.com>
2022-05-27 23:21:56 +0530

    Number of Steps to Reduce a Number to Zero

---
+++ BIT MANIPULATION/Steps to reduce zero.cpp
@@ -0,0 +1,23 @@
+//LeetCode:1342
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        int count = 0;
+        while(num!=0)
+        {
+            if(num%2==0)
+            {
+                num=num/2;
+                count++;
+            }
+             else
+             {
+                 num=num-1;
+                 count++;
+                 
+             }
+                
+        }
+        return count;
+    }
+};
