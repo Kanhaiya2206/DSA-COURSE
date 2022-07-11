2fe58262462645dffd4ba294796bba0740da57a9
kanhaiya <kumar.kanhaiya2206@gmail.com>
2022-07-11 17:55:59 +0530

    0-1 KnapSack

--- DP/0-1 Knapsack.cpp
+++ DP/0-1 Knapsack.cpp
@@ -1,6 +1,7 @@
 
 #include<bits/stdc++.h>
 using namespace std;
+//2. Memoize above recursive code (top-down)
 class Solution
 {
     public:
@@ -35,3 +36,49 @@ class Solution
     }
 };
 
+//3. Bottom Up Approach - Dynamic Programming
+int knapSack(int w, int wt[], int val[], int n) 
+    {
+        int t[n+1][w+1];
+        
+        //Initialize matrix
+        for(int i=0; i<w+1; i++)
+        for(int j=0; j<n+1; j++)
+        {
+          if(i==0 || j==0)
+            t[i][j]=0;
+        }
+        
+        //Choice Diagram to code
+        for(int i = 1; i<n+1; i++){
+            for(int j=1; j<w+1; j++){
+                //1. In case of choice
+                if(wt[i-1] <= j){
+                    t[i][j] = max(val[i-1] + t[i-1][j-wt[i-1]], 0+t[i-1][j]);
+                }
+                //2. No choice
+                else if(wt[i-1] > j){
+                    t[i][j] = 0+t[i-1][j];
+                }
+            }
+        }
+        return t[n][w];
+    }
+
+//1. Recursive Approach
+int knapSack(int w, int wt[], int val[], int n) 
+    { 
+       //Base Case
+       if(n==0 || w==0) return 0;
+       
+        //Choice Diagram Code 
+        //1. Choice to include item or not
+        if(wt[n-1] <= w){
+            return max(val[n-1] + knapSack(w-wt[n-1], wt, val, n-1), 0+knapSack(w,wt,val,n-1));
+        }
+
+       //2. No choice
+        else if(wt[n-1] > w){
+            return 0 + knapSack(w,wt,val,n-1);
+        }
+   }    
