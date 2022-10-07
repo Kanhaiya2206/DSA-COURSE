6677d6d0fee9315eb312a4ae2e05c5150124c4a8
kanhaiya <kumar.kanhaiya2206@gmail.com>
2022-10-07 23:36:59 +0530

    Same Tree

---
+++ BINARY TREE/Same Tree.cpp
@@ -0,0 +1,25 @@
+//LeetCode:100
+#include<bits/stdc++.h>
+using namespace std;
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p==NULL && q!=NULL) return false;
+        if(p!=NULL && q==NULL) return false;
+        if(p==NULL && q==NULL) return true;
+        
+        bool left = isSameTree(p->left,q->left);
+        bool right = isSameTree(p->right,q->right);
+        
+        return p->val==q->val && left && right;
+    }
+};
