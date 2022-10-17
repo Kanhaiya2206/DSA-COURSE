f26c186f89006c6d5876093a91eba0ae10f412d8
kanhaiya <kumar.kanhaiya2206@gmail.com>
2022-10-17 10:37:32 +0530

    Duplicate in Array

---
+++ BIT MANIPULATION/DuplicateInArray.cpp
@@ -0,0 +1,16 @@
+//Duplicates in Array [https://bit.ly/3dm6bdZ ]
+#include<bits/stdc++.h>
+using namespace std;
+int findDuplicate(vector<int> &arr)
+{
+    int ans = 0;
+    for(int i=0; i<arr.size(); i++)
+    {
+        ans = ans ^ arr[i];
+    }
+    for(int i=1; i<arr.size(); i++)
+    {
+        ans = ans ^ i;
+    }
+    return ans;
+}
